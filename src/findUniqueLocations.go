52045a71ec2e303e5e246b45f9ab500e84c1af11
Sam Burns <tech@sam-burns.com>
2019-04-04 13:03:01 +0100

    First working algorithm

--- src/findUniqueLocations.go
+++ src/findUniqueLocations.go
@@ -1,8 +1,38 @@
 package main
 
-import (
-)
+type gridLoc struct {
+	x int
+	y int
+}
+
+var placesVisited = make(map[int]map[int]bool)
 
 func FindUniqueLocations(directionsStr string) int {
-	return len(directionsStr)
+
+	currentLocation := gridLoc{0, 0}
+
+	for _, direction := range directionsStr {
+
+		if direction == 'N' {
+			currentLocation = gridLoc{currentLocation.x, currentLocation.y + 1}
+		} else if direction == 'S' {
+			currentLocation = gridLoc{currentLocation.x, currentLocation.y - 1}
+		} else if direction == 'E' {
+			currentLocation = gridLoc{currentLocation.x + 1, currentLocation.y}
+		} else if direction == 'W' {
+			currentLocation = gridLoc{currentLocation.x - 1, currentLocation.y}
+		}
+
+		visit(currentLocation)
+	}
+
+	return len(placesVisited)
+}
+
+func visit (placeVisited gridLoc) {
+	_, alreadyVisitedXOrdinate := placesVisited[placeVisited.x]
+	if ! alreadyVisitedXOrdinate {
+		placesVisited[placeVisited.x] = make(map[int]bool)
+	}
+	placesVisited[placeVisited.x][placeVisited.y] = true
 }
--- src/findUniqueLocations_test.go
+++ src/findUniqueLocations_test.go
@@ -7,6 +7,15 @@ import (
 func init() {
 }
 
+func TestEmptyString(t *testing.T) {
+	testCase := ""
+	expectedResult := 0
+	result := FindUniqueLocations(testCase)
+	if result != expectedResult {
+		t.Fatalf("Failed counting an empty journey: expected %d, got %d", expectedResult, result)
+	}
+}
+
 func TestASingleMovementCanBeCounted(t *testing.T) {
 	testCase := "N"
 	expectedResult := 1
@@ -21,6 +30,15 @@ func TestMultipleMovementsCanBeCounted(t *testing.T) {
 	expectedResult := 2
 	result := FindUniqueLocations(testCase)
 	if result != expectedResult {
-		t.Fatalf("Failed counting a single move: expected %d, got %d", expectedResult, result)
+		t.Fatalf("Failed counting a two moves: expected %d, got %d", expectedResult, result)
+	}
+}
+
+func TestDuplicateLocationsAreNotCounted(t *testing.T) {
+	testCase := "NEW"
+	expectedResult := 2
+	result := FindUniqueLocations(testCase)
+	if result != expectedResult {
+		t.Fatalf("Failed counting a path where a location is visited twice: expected %d, got %d", expectedResult, result)
 	}
 }
