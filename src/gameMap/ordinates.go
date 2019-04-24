8765caeb841f243c5e11409f4d17c00f9750531c
Sam Burns <tech@sam-burns.com>
2019-04-24 12:19:15 +0100

    micro-optimisations

--- src/gameMap/ordinates.go
+++ src/gameMap/ordinates.go
@@ -3,10 +3,13 @@ package gameMap
 //const MemSize = 50000
 const MemSize = 66000
 
+const binary1OnXAxis uint64 = 4294967296 // 1 << 32
+const binaryOrigin uint64 = 9223372039002259456 // (1 << 63) + (1 << 31)
+
 type OrdinatesVisitedType [MemSize]uint64
 
 func GenerateOrigin() uint64 {
-	return (1 << 63) + (1 << 31)
+	return binaryOrigin
 }
 
 func MoveNorth(location *uint64) {
@@ -18,11 +21,11 @@ func MoveSouth(location *uint64) {
 }
 
 func MoveEast(location *uint64) {
-	*location += 1 << 32
+	*location += binary1OnXAxis
 }
 
 func MoveWest(location *uint64) {
-	*location -= 1 << 32
+	*location -= binary1OnXAxis
 }
 
 func (ordinatesVisited *OrdinatesVisitedType) Clobber(inputStringLength int) {
